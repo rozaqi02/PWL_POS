48e672760551fc2ab0a49a7d150c450884992a68
rozaqi02 <rojaki1419@gmail.com>
2024-11-06 13:40:15 +0700

    pertemuan 10 praktikum 4

---
+++ app/Http/Controllers/Api/LevelController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\LevelModel;
+use Illuminate\Http\Request;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        return LevelModel::all();
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $level = LevelModel::create($request->all());
+        return response()->json($level, 201);
+    }
+
+    public function show(LevelModel $level)
+    {
+        return LevelModel::find($level);
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, LevelModel $level)
+    {
+        $level->update($request->all());
+        return LevelModel::find($level);
+    }
+
+    public function destroy(LevelModel $user)
+    {
+        $user->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Terhapus',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\SupplierController;
+use App\Http\Controllers\Api\StokController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +27,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
